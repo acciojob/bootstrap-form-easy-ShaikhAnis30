f80bd7a389195761a28a9caf965caba4971f602c
ShaikhAnis30 <anisshaikh8535@gmail.com>
2023-07-10 20:32:05 +0530

    Solution

--- index.js
+++ index.js
@@ -13,6 +13,29 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/main.html'));
 });
 //your code here
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+
 app.post('/add', (req, res) => {
   const {a,b} = req.body;
   res.status(200).send(a+b);
